081d5aeef4bad129eb07f2963bbcdf537ed39d3e
Grant Walker <gwalker@mines.edu>
2013-05-23 08:25:02 -0600

    Removed mongostuff, added new folder test_mongo

--- test_mongo/test_mongo.rb
+++ test_mongo/test_mongo.rb
@@ -1,8 +1,12 @@
 require 'mongo'
-require 'rubygems'
 
 include Mongo
 
+mongo_client = MongoClient.new("localhost", 9393)
+db = mongo_client.db("mydb")
+coll = db["testCollection"]
+doc = {"name" => "MongoDB","type" => "database", "count" => 1}
+id = coll.insert(doc)
 
 # @client = MongoClient.new('localhost', 27017)
 # @db = @client['sample-db']
@@ -17,12 +21,12 @@ include Mongo
 # puts "There are #{@coll.count} records. Here they are: "
 # @coll.find.each { |doc| puts doc.inspect }
 
-class Mongostuff
+# class Mongostuff
 
-	attr_accessor :id
+# 	attr_accessor :id
 
-	def initialize(id)
-		@id = id
-	end
+# 	def initialize(id)
+# 		@id = id
+# 	end
 
-end
+# end
