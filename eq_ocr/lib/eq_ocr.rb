273f48346835e10e8000246c7fadeb5571fe0176
snowe2010 <tylerbthrailkill@gmail.com>
2013-05-24 10:45:23 -0600

    Removed old functions from main sinatra file

--- eq_ocr/lib/eq_ocr.rb
+++ eq_ocr/lib/eq_ocr.rb
@@ -15,69 +15,31 @@ module Eq
 
       post '/upload_bitmap' do
 
-        #Putting mongo things here
-        # mongo_client = MongoClient.new("localhost", 27017)
-        # db = mongo_client.db("mydb")
-        # coll = db["testCollection"]
-        # doc = {"name" => "MongoDB","type" => "database", "count" => 1}
-        # id = coll.insert(doc)
-        #end mongo things
-        new_request = request.body.read.split("[")
+        # new_request = request.body.read.split("[")
 
-        #s3 = AwsInstance.new
+        s3 = AwsInstance.new
         File.open('/tmp/file_1', 'wb') { |f| f.write(request.body.read)}
-        #s3.upload_file('/tmp/file_1')
+        s3.upload_file('/tmp/file_1')
 
         status 200
         body "1"
       end
 
-      get '/loadprompt' do
-        erb :loader
-      end
-
-      get '/saveprompt' do
-        erb :saver
-      end
-      
-      post '/savefile' do
-        # verify appropriate bucket
-        # save file
-        # return save status
-        name = params[:upfile][:filename]
-        upfile = params[:upfile][:tempfile]
-        File.open('/tmp/'+name, "wb") { |f| f.write(upfile.read) }
-        "SUCCESS"
-      end
-
-      get '/loadfile' do
-        file_name = params['filename']
-        ## aws api to server
-        # check for file existance
-        # load file
-        # return file object
-        if !File.exists?('/tmp/'+file_name)
-          "Error Loading File"
-        else
-          file = File.open('/tmp/'+file_name, "r")
-        end
-      end 
-
       post '/db_update' do
         new_request = request.body.read
 
-         @client = MongoClient.new('127.0.0.1', 27017)
-         @db     = @client['sample-db']
-         @coll   = @db['test']
+        @client = MongoClient.new('127.0.0.1', 27017)
+        @db     = @client['sample-db']
+        @coll   = @db['test']
 
-         @coll.remove
+        @coll.remove
 
-         3.times do |i|
-          @coll.insert({'a' => i+1})
-         end
+        3.times do |i|
+         @coll.insert({'a' => i+1})
+        end
 
-         puts "There are #{@coll.count} records. Here they are:"
-         @coll.find.each { |doc| puts doc.inspect }
+        puts "There are #{@coll.count} records. Here they are:"
+        @coll.find.each { |doc| puts doc.inspect }
           
         status 200
         ocr = OcrExt.new
