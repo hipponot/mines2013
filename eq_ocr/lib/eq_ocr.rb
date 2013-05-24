d2b355dc46a28b600a072b8054eee13afcd04293
snowe2010 <tylerbthrailkill@gmail.com>
2013-05-24 10:59:08 -0600

    Updated paths and added helper methods

--- eq_ocr/lib/eq_ocr.rb
+++ eq_ocr/lib/eq_ocr.rb
@@ -13,37 +13,47 @@ module Eq
         "<form action=\"/upload_bitmap\" method=\"post\"><input type=\"text\" name=\"something\"><input type=\"submit\" value=\"submit\"></form>"
       end
 
-      post '/upload_bitmap' do
 
-        # new_request = request.body.read.split("[")
-
-        s3 = AwsInstance.new
-        File.open('/tmp/file_1', 'wb') { |f| f.write(request.body.read)}
-        s3.upload_file('/tmp/file_1')
-
-        status 200
-        body "1"
+      post '/ocr' do
+        upload_bitmap
+        db_update
       end
 
-      post '/db_update' do
-        new_request = request.body.read
+      helpers do
 
-        @client = MongoClient.new('127.0.0.1', 27017)
-        @db     = @client['sample-db']
-        @coll   = @db['test']
+        def upload_bitmap
 
-        @coll.remove
+          # new_request = request.body.read.split("[")
 
-        3.times do |i|
-         @coll.insert({'a' => i+1})
+          s3 = AwsInstance.new
+          File.open('/tmp/file_1', 'wb') { |f| f.write(request.body.read)}
+          s3.upload_file('/tmp/file_1')
+
+          status 200
+          body "1"
+        end
+        
+        def db_update
+          new_request = request.body.read
+
+          @client = MongoClient.new('127.0.0.1', 27017)
+          @db     = @client['sample-db']
+          @coll   = @db['test']
+
+          @coll.remove
+
+          3.times do |i|
+           @coll.insert({'a' => i+1})
+          end
+
+          puts "There are #{@coll.count} records. Here they are:"
+          @coll.find.each { |doc| puts doc.inspect }
+            
+          status 200
+          ocr = OcrExt.new
+          body "#{ocr.recognize_char}"
         end
 
-        puts "There are #{@coll.count} records. Here they are:"
-        @coll.find.each { |doc| puts doc.inspect }
-          
-        status 200
-        ocr = OcrExt.new
-        body "#{ocr.recognize_char}"
       end
     end
   end
