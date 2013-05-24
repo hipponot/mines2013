8a0a13a51ddf95f1ee70d152926e3ac3c36cb983
Austin Thompson <Austin@wl-dhcp194-174.mines.edu>
2013-05-24 11:17:45 -0600

    Removed unnecessary folders test_foo, foo and tmp

--- tmp/test_blah/lib/test_blah/version.rb
+++
@@ -1,8 +0,0 @@
-require 'yaml'
-module Test
-module Blah
-  kudu = YAML::load(IO.read(File.join(File.dirname(__FILE__), '../../kudu.yaml')))
-  VERSION = kudu[:publications][0][:version]
-  NAME = kudu[:publications][0][:name]
-end
-end
